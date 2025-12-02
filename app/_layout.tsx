45af5ceebcaa8bf8826cf3084dfcc6fc0d2ab2d5
FranklinUTA <fxv2149@mavs.uta.edu>
2025-12-02 09:48:45 -0600

    Fixed signup layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout() {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
         <Stack.Screen name="friend-profile" options={{ headerShown: false }} />
+        <Stack.Screen name="signup" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
